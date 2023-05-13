598aec60ad5baa69f71ca60e5bcab84345989665
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-05-13 13:40:54 -0300

    add topbar story

--- src/stories/components/Topbar.stories.js
+++ src/stories/components/Topbar.stories.js
@@ -4,7 +4,15 @@ export default {
   title: 'Components/Topbar',
   component: Topbar,
   tags: ['autodocs'],
+  argTypes: {
+    name: { control: 'text' },
+    color: { control: 'color' },
+  }
 }
 
 export const Primary = {
+  args: {
+    name: 'Jhon Doe',
+    color: '#47d5bc',
+  }
 }
