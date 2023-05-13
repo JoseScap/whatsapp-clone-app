3d610973685a00ffd06d353c96d847c89c912b77
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-05-13 01:27:06 -0300

    init topbar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,14 @@
+import Avatar from './Avatar'
+
+function Topbar() {
+  return <div style={{
+    border: '1px solid black'
+  }}>
+    <Avatar
+      image="https://yt3.googleusercontent.com/ytc/AGIKgqNGxs8XAfudsOFr9KhpeQ_JwoIcvSJFLVRQeASp9g=s900-c-k-c0x00ffffff-no-rj"
+      alt="Vercel's Logo"    
+    />
+  </div>
+}
+
+export default Topbar
---
+++ src/stories/components/Topbar.stories.js
@@ -0,0 +1,10 @@
+import { Topbar } from '../../components';
+
+export default {
+  title: 'Components/Topbar',
+  component: Topbar,
+  tags: ['autodocs'],
+}
+
+export const Primary = {
+}
