8894505c534610ec80d3143a7915096180463b84
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-05-15 01:25:17 -0300

    change profile mock

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -9,31 +9,31 @@ export const chatList = [
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#25D366',
     name: 'Jack Doe',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#833AB4',
     name: 'Marilyn Manson',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#c8232c',
     name: 'Buzz Aldrin',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#0072b1',
     name: 'Elon Musk',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#F0DB4F',
     name: 'Isaac Newtwon',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
@@ -45,31 +45,31 @@ export const chatList = [
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#25D366',
     name: 'Jack Doe',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#833AB4',
     name: 'Marilyn Manson',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#c8232c',
     name: 'Buzz Aldrin',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#0072b1',
     name: 'Elon Musk',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {
     uuid: v4(),
-    avatarColor: '#ff4785',
+    avatarColor: '#F0DB4F',
     name: 'Isaac Newtwon',
     lastMessage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
